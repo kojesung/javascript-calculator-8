e3ce9eabf60141cff183fb6169c92ee0c4009beb
kojesung <js95112345@gmail.com>
2025-10-15 01:15:04 +0900

    feat(Parser): 빈 문자열 처리
     빈 문자열 입력 시 빈 배열을 반환하도록 구현하였으며 기능 목록별 반환 항목에 대한 내용을 더 명확하게 수정했습니다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
+import Parser from './Parser.js';
 
 class App {
+    constructor() {
+        this.parser = new Parser();
+    }
     async run() {
         const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+        const parsingResult = this.parser.parse(input);
+        Console.print(`결과 : ${parsingResult}`);
     }
 }
 
---
+++ src/Parser.js
@@ -0,0 +1,8 @@
+class Parser {
+    parse(input) {
+        if (!input) return []; // TODO null, undefined 예외처리
+        if (input === '') return [];
+    }
+}
+
+export default Parser;
