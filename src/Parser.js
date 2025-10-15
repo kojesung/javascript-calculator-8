860328c01b87341d0f547a3570af80d855c23901
kojesung <js95112345@gmail.com>
2025-10-16 01:30:18 +0900

    feat(Parser): 구분자를 제외한 숫자 문자열 추출
    커스텀 구분자가 있을 떈 \n의 index를 찾고 index로부터 문자열의 자를 위치를 판단하였습니다. 커스텀 구분자가 없을 땐 정규식을 이용해 ,와 : 모두를 한번에 구분할 수 있도록 했습니다.

--- src/Parser.js
+++ src/Parser.js
@@ -2,11 +2,19 @@ class Parser {
     parse(input) {
         if (!input) return []; // TODO null, undefined 예외처리
         if (input === '') return [];
-        return this.getCustomSeparator(input);
+        return this.extractNumbersString(input);
     }
     getCustomSeparator(input) {
         const match = input.match(/^\/\/(.+?)(?:\\n|\n)/);
         return match ? match[1] : null;
+    }
+    extractNumbersString(input) {
+        const separator = this.getCustomSeparator(input);
+        if (separator) {
+            const numbersString = input.substring(input.indexOf('\\n') + 2);
+            return numbersString.split(separator);
+        }
+        return input.split(/[,:]/);
     }
 }
 
