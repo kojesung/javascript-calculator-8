ee616c8f4681be8f9921f8e3161a2e31908a66aa
kojesung <js95112345@gmail.com>
2025-10-15 23:17:32 +0900

    feat(Parser): 커스텀 구분자 추출
    //와 \n 사이 구분자를 반환하는 함수를 만들었습니다. 추후에 반환값에 따른 분기 처리를 할 수 있도록 결과에 따라 truthy한 값과 falsy한 값을 반환하도록 설계하였습니다.

--- src/Parser.js
+++ src/Parser.js
@@ -2,6 +2,11 @@ class Parser {
     parse(input) {
         if (!input) return []; // TODO null, undefined 예외처리
         if (input === '') return [];
+        return this.getCustomSeparator(input);
+    }
+    getCustomSeparator(input) {
+        const match = input.match(/^\/\/(.+?)(?:\\n|\n)/);
+        return match ? match[1] : null;
     }
 }
 
