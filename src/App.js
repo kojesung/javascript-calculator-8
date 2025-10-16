932ef58fb6cb7b75a89504afe7453ab7c7b1c2a1
kojesung <js95112345@gmail.com>
2025-10-16 21:48:22 +0900

    feat(Output): 계산기 결과 출력 처리
    Console.print()를 활용하여 과제에서 제시한 형식에 일치하는 형태로 계산기 결과를 출력하도록 하였습니다.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App {
     async run() {
         const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
         const parsingResult = this.parser.parse(input);
-        Console.print(`결과 : ${parsingResult}`);
+        const calculateResult = this.calculator.sumListElements(parsingResult);
+        Console.print(`결과 : ${calculateResult}`);
     }
 }
 
