538f49a8ad8f0f0e0e1da39e6eaa2a24d50847d8
kojesung <js95112345@gmail.com>
2025-10-20 11:33:41 +0900

    feat(Calculator): sum 메서드에 잘못된 인자 전달에 대한 예외 처리
    Calculator.sum() 메서드를 다른 곳에서 사용할 때 배열이 아닌 값을 전달하거나, 배열에 숫자가 아닌 값이 포함되어 있을 때 명시적으로 에러를 발생시킵니다.

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,13 @@
 class Calculator {
     sumListElements(list) {
+        if (!Array.isArray(list)) {
+            throw new Error('[ERROR] 배열이 아닙니다.');
+        }
+
+        if (!list.every((n) => typeof n === 'number' && !isNaN(n))) {
+            throw new Error('[ERROR] 숫자가 아닌 요소가 있습니다.');
+        }
+
         if (list.length === 0) {
             return 0;
         }
